b9a361ed19b119e1d94ac53f700b1b0d75ac7d79
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2025-08-18 18:10:25 +0100

    Added welcome mail email templates

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -3,15 +3,10 @@ from app.services.notification_service import NotificationService
 
 
 class EmailService:
-    def __init__(self, notification_service: NotificationService):
-        self.notification_service = notification_service
+	def __init__(self, notification_service: NotificationService):
+		self.notification_service = notification_service
 
-    async def send_welcome_email(self, user) -> None:
-        subject = "Welcome to Platepal"
-        body = (
-            f"Hello {user.first_name},\n\n"
-            "Thank you for joining Platepal, your go-to food ordering app! "
-            "We're excited to have you on board and look forward to serving you delicious meals.\n\n"
-            "Best regards,\nThe Platepal Team"
-        )
-        await self.notification_service.send_email([user.email], subject, body)
+	async def send_welcome_email(self, user) -> None:
+		subject = "Welcome to Platepal"
+
+		await self.notification_service.send_email(user, subject, 'welcome_mail.html')
--- app/services/notification_service.py
+++ app/services/notification_service.py
@@ -1,22 +1,30 @@
+from pathlib import Path
+
 from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
 from pydantic import EmailStr
-
 from app.config.env_config import notification_setting
+from app.models.user import User
+
+TEMPLATE_DIR = Path('app', 'templates')
+
 
 class NotificationService:
-    def __init__(self):
-        self.fast_mail = FastMail(
-            ConnectionConfig(
-                **notification_setting.model_dump()
-            )
-        )
+	def __init__(self):
+		self.fast_mail = FastMail(
+			ConnectionConfig(
+				**notification_setting.model_dump(),
+				TEMPLATE_FOLDER=TEMPLATE_DIR,
+			)
+		)
 
-    async def send_email(self, recipients: list[EmailStr], subject: str, body: str):
-        message = MessageSchema(
-            recipients=recipients,
-            subject=subject,
-            body=body,
-            subtype=MessageType.plain
-        )
-        await self.fast_mail.send_message(message)
-
+	async def send_email(self, user: User, subject: str, template_name: str) -> None:
+		message = MessageSchema(
+			recipients=[user.email],
+			subject=subject,
+			subtype=MessageType.html,
+			template_body={
+				"user_name": user.first_name,
+				"user_email": user.email,
+			}
+		)
+		await self.fast_mail.send_message(message, template_name=template_name)
